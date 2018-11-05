90a76ace3de5c216a63c5b8b053374a7a6d1823b
Fernando <fer.valdes23@gmail.com>
2018-11-05 13:49:02 -0600

    -redifined how pieces are declared -finished basic movement function -wrapped checkOccupied in try-catch -started with checkPossibleMoves function

--- checkers.py
+++ checkers.py
@@ -5,18 +5,36 @@ class Board:
 		#initialize 2d array populated by 'None' in all fields
 		self.board = [[None for i in range(8)] for j in range(8)]
 		#spots where red pieces start
-		self.startRed = [(1,2), (1,4), (1,6), (1,8), (2,1), 
-			(2,3), (2,5), (2,7), (3,2), (3,4) , (3,6) , (3,8)]
+		self.startRed = [(0,1), (0,3), (0,5), (0,7), (1,0), 
+			(1,2), (1,4), (1,6), (2,1), (2,3) , (2,5) , (2,7)]
 		#spots where black pieces start
-		self.startblack = [(8,1), (8,3), (8,5), (8,7), (7,2),
-			(7,4), (7,6), (7,8), (6,1), (6,3), (6,5), (6,7)]
+		self.startblack = [(7,0), (7,2), (7,4), (7,6), (6,1),
+			(6,3), (6,5), (6,7), (5,0), (5,2), (5,4), (5,6)]
+
+		self.pieces = {
+		}
 
 	def populateboard(self):
 		#'element[index] - 1' to keep inside bounds of 8x8 
+
 		for element in (self.startblack):
-			self.board[element[0] - 1][element[1] - 1] = 'B' #black
+			self.pieces[element] = {
+				'team' : 'black',
+				'position' : element,#black,
+				'king' : False,
+				'valid': True #if piece is still in play or not
+			}
+			self.board[element[0]][element[1]] = self.pieces[element] #black
+
 		for element in (self.startRed):
-			self.board[element[0] - 1][element[1] - 1] = 'R' #red
+			self.pieces[element] = {
+				'team' : 'red',
+				'position' : element,	#red,
+				'king' : False,
+				'valid': True #if piece is still in play or not
+			}
+			self.board[element[0]][element[1]] = self.pieces[element] #red
+
 
 	def printboard(self):
 		print("-"*40)
@@ -25,75 +43,86 @@ class Board:
 				if (self.board[col][row] == None):
 					print(" | ", ' ' , end='')
 				else:
-					print(" | ", self.board[col][row], end='')
+					print(" | ", self.board[col][row]['team'], end='')
 			print(" |",'\n')
-			print("-"*42)
+			print("-"*42)				
 
-		#print(board) to command line
-		# black_circle = '\N{black circle}'
-		# black_circle_utf8 = black_circle.decode('utf8')
-				
+def movement(board, pieces, currCoordinate, newCoordinate):
+
+	assert checkEdge(newCoordinate)
+	assert checkOccupied(board, newCoordinate)
+
+	board[newCoordinate[0]][newCoordinate[1]] = pieces[currCoordinate]
+	board[currCoordinate[0]][currCoordinate[1]] = None	
 
 
-class pieces:
-	# team = {
-	# 	'R' = 'red',
-	# 	'B' = 'blue'
-	# }
+def capture(board, pieces, currCoordinate, newCoordinate):
+	assert checkEdge(newCoordinate)
+	assert checkOccupied(board, newCoordinate)
 
-	# piece = {
-	# 'b' : 'blue',
-	# 'bk': 'blue king',
-	# 'r' : 'red',
-	# 'rk': 'red king'
-	# }
 	pass
 
-class actions:
-	def movement(self, board, currCoordinate, newCoordinate):
 
-		assert checkEdge(newCoordinate)
-		assert board[newCoordinate[0] - 1][newCoordinate[1] - 1] == None
-
-		if team == 'red':
-			#move down
-			#check if not occupied
-			board[newCoordinate[0] - 1][newCoordinate[1] - 1] = 'R' 
-			board[currCoordinate[0] - 1][currCoordinate[1] - 1] = None
-			pass
-		elif team == 'blue':
-			#move up
-			#check if not going to edge
-			#check if not occupied
-			pass
-		else:
-			#king so can move either up and down
-			#check if not going to edge
-			#check if not occupied
-			pass
-
-	def capture():
-		#check if not going to edge
-		#check if place over is not occupied
-		pass
-
-	def checkEdge(newCoordinate):
-		#returns true if going out of bounds
-		return ((newCoordiante[0] < 8) and (newCoordinate[1] > 0))
+def checkEdge(newCoordinate):
+	#returns true if going out of bounds
+	return ((newCoordinate[0] < 8) and (newCoordinate[1] > 0))
 
 
-class checkPossibleMoves:
-	pass
+	#returns true if new coordinate is already populated
+def checkOccupied(board, newCoordinate):
+	try:
+		return (board[newCoordinate[0]][newCoordinate[1]] == None)
+	except:
+		return False
+
+def checkPossibleMoves(board, pieces):
+	
+	moves = {}
+
+	for i in pieces:
+		if pieces[i]['team'] == 'red' and pieces[i]['valid'] == True:
+
+			coordinate = pieces[i]['position']
+
+			#check down left
+			coordinateCheck = (coordinate[0] - 1,coordinate[1] - 1)
+			if checkEdge(coordinateCheck) and checkOccupied(board, coordinateCheck):
+				moves[i] = {
+					'team' : 'red',
+					'old-position' : coordinate,
+					'new-position' : (coordinate[0] + 1 , coordinate[1] - 1)
+				}
+				print ("red")
+			#check down right
+			coordinateCheck = (coordinate[0] - 1,coordinate[1] + 1)
+			if checkEdge(coordinateCheck) and checkOccupied(board, coordinateCheck):
+				moves[i] = {
+					'team' : 'red',
+					'old-position' : coordinate,
+					'new-position' : (coordinate[0] + 1 , coordinate[1] + 1)
+				}
+				print ("red")		
+
+		elif pieces[i]['team'] == 'black' and pieces[i]['valid'] == True:
+			#print ("black")
+			#check up left
+			#check up right
+
+	print(moves)
+	return moves
+
 
 if __name__ == "__main__":
 	b = Board()
 	b.populateboard()
 	b.printboard()
 
+	checkPossibleMoves(b.board, b.pieces)
+
+	movement(b.board, b.pieces, (5,4), (4,5))
+	b.printboard()
+
 
 
 	# test = input("write something: ")
 	# print(test)
-
-#":black_circle:"
-#":red_circle:"
